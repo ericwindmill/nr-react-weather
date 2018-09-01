522d618ffae84189102f998d80dd62cfb0c857bc
Eric Windmill <eric@ericwindmill.com>
2018-09-01 07:58:49 -0700

    add blocs and models for planning

--- src/blocs/weather_bloc.js
+++ src/blocs/weather_bloc.js
@@ -1,3 +1,11 @@
 /*
-
+BLoC (Business Logic Component), is essentially the front-end
+version of a 'Controller'. Views that need to interact with any
+weather related data/logic will call out to this object.
  */
+
+export default class WeatherBloc {
+  constructor() {
+
+  }
+}
